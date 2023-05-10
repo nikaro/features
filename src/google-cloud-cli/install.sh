b93224636d155524ed9d24e9c66a6c8d0fc25b7c
Nicolas Karolak <nicolas@karolak.fr>
2023-05-10 21:58:12 +0000

    add gcloud

---
+++ src/google-cloud-cli/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+set -o errexit # Exit on error
+set -o nounset # Exit on uninitialized variable
+if [ "${1:-}" = "DEBUG" ]; then
+  set -o xtrace
+fi
+
+# get latest version
+if [ -z "${VERSION:-}" ]; then
+  echo "version required"
+  exit 1
+fi
+
+case "$(uname -m)" in
+x86_64)
+  ARCHITECTURE="x86_64"
+  ;;
+aarch64 | armv8* | arm64)
+  ARCHITECTURE="arm"
+  ;;
+*)
+  echo "Architecture unsupported"
+  exit 1
+  ;;
+esac
+
+curl -sSL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-${VERSION}-linux-${ARCHITECTURE}.tar.gz -o /tmp/gcloud.tar.gz
+tar -xzf /tmp/gcloud.tar.gz -C /tmp
+mv /tmp/google-cloud-sdk /opt/
+
+INSTALL_ARGS="--quiet"
+[ -n "${USAGE_REPORTING:-}" ] && INSTALL_ARGS="${INSTALL_ARGS} --usage-reporting ${USAGE_REPORTING:-}"
+[ -n "${RC_PATH:-}" ] && INSTALL_ARGS="${INSTALL_ARGS} --rc-path ${RC_PATH:-}"
+[ -n "${OVERRIDE_COMPONENTS:-}" ] && INSTALL_ARGS="${INSTALL_ARGS} --overide-components ${OVERRIDE_COMPONENTS:-}"
+[ -n "${ADDITIONAL_COMPONENTS:-}" ] && INSTALL_ARGS="${INSTALL_ARGS} --additional-components ${ADDITIONAL_COMPONENTS:-}"
+
+# shellcheck disable=SC2086
+/opt/google-cloud-sdk/install.sh $INSTALL_ARGS
