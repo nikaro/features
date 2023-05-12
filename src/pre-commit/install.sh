844084307e836c81c900048c08e5943e235e1211
Nicolas Karolak <nicolas@karolak.fr>
2023-05-12 08:58:41 +0000

    feat: add pre-commit

---
+++ src/pre-commit/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -o errexit # Exit on error
+set -o nounset # Exit on uninitialized variable
+
+# get latest version
+if [ -z "${VERSION:-}" ]; then
+    VERSION="$(curl -s https://api.github.com/repos/pre-commit/pre-commit/releases/latest | jq -r '.tag_name' | sed 's/v//')"
+fi
+
+curl -sSL "https://github.com/pre-commit/pre-commit/releases/download/v${VERSION}/pre-commit-${VERSION}.pyz" -o /usr/local/bin/pre-commit
+chmod 0755 /usr/local/bin/pre-commit
--- test/pre-commit/test_version.sh
+++ test/pre-commit/test_version.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "ensure is installed" shfmt --version
+check "ensure version" pre-commit --version | grep "3.3.0"
 
 reportResults
