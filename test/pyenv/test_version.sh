98792e2fb255691dcdce6481084df91b1e10f8e3
Nicolas Karolak <nicolas@karolak.fr>
2023-05-19 11:57:40 +0000

    fix(pyenv): test version

--- test/pyenv/test_version.sh
+++ test/pyenv/test_version.sh
@@ -5,6 +5,6 @@ set -e
 source dev-container-features-test-lib
 
 check "ensure version" pyenv --version | grep "2.3.16"
-check "ensure is installed" python --version | grep "3.10.11"
+check "ensure is installed" python --version | grep "3.10"
 
 reportResults
