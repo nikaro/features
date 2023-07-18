2412e22dbd056f401580ca99e0809707c083ecb6
Nicolas Karolak <nicolas@karolak.fr>
2023-07-18 14:28:57 +0000

    feat(shfmt): add settings for ext

--- src/shfmt/install.sh
+++ src/shfmt/install.sh
@@ -5,20 +5,20 @@ set -o nounset # Exit on uninitialized variable
 
 # get latest version
 if [ -z "${VERSION:-}" ]; then
-    VERSION="$(curl -s https://api.github.com/repos/mvdan/sh/releases/latest | jq -r '.tag_name' | sed 's/v//')"
+  VERSION="$(curl -s https://api.github.com/repos/mvdan/sh/releases/latest | jq -r '.tag_name' | sed 's/v//')"
 fi
 
 case "$(uname -m)" in
 x86_64)
-    ARCHITECTURE="amd64"
-    ;;
+  ARCHITECTURE="amd64"
+  ;;
 aarch64 | armv8* | arm64)
-    ARCHITECTURE="arm64"
-    ;;
+  ARCHITECTURE="arm64"
+  ;;
 *)
-    echo "Architecture unsupported"
-    exit 1
-    ;;
+  echo "Architecture unsupported"
+  exit 1
+  ;;
 esac
 
 curl -sSL "https://github.com/mvdan/sh/releases/download/v${VERSION}/shfmt_v${VERSION}_linux_${ARCHITECTURE}" -o /usr/local/bin/shfmt
