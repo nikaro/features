34b843814d9661608bb1f1e35c63bcf37b67828d
Nicolas Karolak <nicolas@karolak.fr>
2024-01-27 23:47:19 +0000

    feat: add npm-package feature

---
+++ src/npm-package/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+set -o errexit # Exit on error
+set -o nounset # Exit on uninitialized variable
+if [ "${DEBUG:-}" = "true" ]; then
+  set -o xtrace
+fi
+
+# shellcheck source=library_scripts.sh
+. ./library_scripts.sh
+
+ensure_nanolayer
+
+nanolayer install devcontainer-feature "ghcr.io/nikaro/features/node"
+
+VERSION=${VERSION:-"latest"}
+
+if ! npm list --short | grep -q "$PACKAGE"; then
+  if [ "$VERSION" = "latest" ]; then
+    npm_installation="$PACKAGE"
+  else
+    npm_installation="$PACKAGE@$VERSION"
+  fi
+
+  # install package
+  npm install -g "$npm_installation"
+fi
+
+remove_nanolayer
