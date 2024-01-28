d6838a89e281b1a4d710fc3dd716cb383c089f43
Nicolas Karolak <nicolas@karolak.fr>
2024-01-28 15:31:48 +0000

    feat: add some go packages feature

---
+++ src/go-pkg-gomodifytags/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -o errexit # Exit on error
+set -o nounset # Exit on uninitialized variable
+if [ "${DEBUG:-}" = "true" ]; then
+  set -o xtrace
+fi
+
+# shellcheck source=library_scripts.sh
+. ./library_scripts.sh
+
+ensure_nanolayer
+nanolayer install devcontainer-feature "ghcr.io/nikaro/features/go-package" \
+  --option package="github.com/fatih/gomodifytags" \
+  --option version="${VERSION:-}" \
+  --option debug="${DEBUG:-}"
+remove_nanolayer
