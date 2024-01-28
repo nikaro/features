113065ebc9f574a8f3b72b51a6ef0edff41d2702
Nicolas Karolak <nicolas@karolak.fr>
2024-01-28 17:41:08 +0000

    fix(staticcheck): correct url

--- src/go-pkg-staticcheck/install.sh
+++ src/go-pkg-staticcheck/install.sh
@@ -11,7 +11,7 @@ fi
 
 ensure_nanolayer
 nanolayer install devcontainer-feature "ghcr.io/nikaro/features/go-package" \
-  --option package="honnef.co/go/tools/staticcheck" \
+  --option package="honnef.co/go/tools/cmd/staticcheck" \
   --option version="${VERSION:-}" \
   --option debug="${DEBUG:-}" \
   --option keep="${KEEP:-}"
