703c99b71c897362ba41fefde7b82889570dafe8
Nicolas Karolak <nicolas@karolak.fr>
2024-01-28 17:31:53 +0000

    fix(goplay): use correct url

--- src/go-pkg-goplay/install.sh
+++ src/go-pkg-goplay/install.sh
@@ -11,7 +11,7 @@ fi
 
 ensure_nanolayer
 nanolayer install devcontainer-feature "ghcr.io/nikaro/features/go-package" \
-  --option package="github.com/haya14busa/goplay" \
+  --option package="github.com/haya14busa/goplay/cmd/goplay" \
   --option version="${VERSION:-}" \
   --option debug="${DEBUG:-}" \
   --option keep="${KEEP:-}"
