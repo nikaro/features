eaa17fa68c150a28d297322cb2a110f2b687cf9d
Nicolas Karolak <nicolas@karolak.fr>
2024-02-13 23:16:40 +0000

    feat(tflint): simplify install

--- src/tflint/install.sh
+++ src/tflint/install.sh
@@ -2,31 +2,24 @@
 
 set -o errexit # Exit on error
 set -o nounset # Exit on uninitialized variable
-if [ "${1:-}" = "DEBUG" ]; then
+if [ "${DEBUG:-}" = "true" ]; then
   set -o xtrace
 fi
 
+# shellcheck source=library_scripts.sh
+. ./library_scripts.sh
+
 # get latest version
 if [ -z "${VERSION:-}" ]; then
-  VERSION="$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | jq -r '.tag_name' | sed 's/v//')"
+  VERSION="$(get_latest_gh_release terraform-linters/tflint)"
 fi
 
-case "$(uname -m)" in
-x86_64)
-  ARCHITECTURE="amd64"
-  ;;
-aarch64 | armv8* | arm64)
-  ARCHITECTURE="arm64"
-  ;;
-*)
-  echo "Architecture unsupported"
-  exit 1
-  ;;
-esac
-
-curl -sSL "https://github.com/terraform-linters/tflint/releases/download/v${VERSION}/tflint_linux_${ARCHITECTURE}.zip" -o /tmp/tflint.zip
-unzip /tmp/tflint.zip tflint -d /tmp/
-mv /tmp/tflint /usr/local/bin/tflint
-chmod 0755 /usr/local/bin/tflint
-
-rm -rf /tmp/tflint.zip
+# install if needed
+if ! tflint --version | grep -q "$VERSION"; then
+  FILENAME="tflint_linux_$(get_arch64_simple).zip"
+  URL="https://github.com/terraform-linters/tflint/releases/download/v${VERSION}/${FILENAME}"
+  curl -LO "$URL"
+  unzip "${FILENAME}" tflint -d /usr/local/bin
+  chmod 0755 /usr/local/bin/tflint
+  rm -rf "${FILENAME}"
+fi
