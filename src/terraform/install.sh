ae5ac8f36f13aba388b7adb8c6e02f092d663771
Nicolas Karolak <nicolas@karolak.fr>
2024-02-13 12:00:49 +0000

    feat(terraform): update script and settings

--- src/terraform/install.sh
+++ src/terraform/install.sh
@@ -2,31 +2,24 @@
 
 set -o errexit # Exit on error
 set -o nounset # Exit on uninitialized variable
-if [ "${1:-}" = "DEBUG" ]; then
+if [ "${DEBUG:-}" = "true" ]; then
   set -o xtrace
 fi
 
+# shellcheck source=library_scripts.sh
+. ./library_scripts.sh
+
 # get latest version
 if [ -z "${VERSION:-}" ]; then
-  VERSION="$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | jq -r '.tag_name' | sed 's/v//')"
+  VERSION="$(get_latest_gh_release hashicorp/terraform)"
 fi
 
-case "$(uname -m)" in
-x86_64)
-  ARCHITECTURE="amd64"
-  ;;
-aarch64 | armv8* | arm64)
-  ARCHITECTURE="arm64"
-  ;;
-*)
-  echo "Architecture unsupported"
-  exit 1
-  ;;
-esac
-
-curl -sSL "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_${ARCHITECTURE}.zip" -o /tmp/terraform.zip
-unzip /tmp/terraform.zip terraform -d /tmp/
-mv /tmp/terraform /usr/local/bin/terraform
-chmod 0755 /usr/local/bin/terraform
-
-rm -rf /tmp/terraform.zip
+# install if needed
+if ! terraform version | grep -q -e "$VERSION"; then
+  FILENAME="terraform_${VERSION}_linux_$(get_arch64_simple).zip"
+  URL="https://releases.hashicorp.com/terraform/${VERSION}/${FILENAME}"
+  curl -LO "$URL"
+  unzip "${FILENAME}" terraform -d /usr/local/bin
+  chmod 0755 /usr/local/bin/terraform
+  rm -rf "${FILENAME}"
+fi
